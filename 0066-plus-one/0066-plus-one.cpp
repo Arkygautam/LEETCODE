1fb82a802612c75d92cc7381954a67b711472859
Arkygautam <117636657+Arkygautam@users.noreply.github.com>
2024-03-02 17:22:08 +0530

    Time: 0 ms (100.00%), Space: 10.2 MB (60.83%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        vector<int> ans;
+        int carry = 0; 
+        for(int i=digits.size()-1;i>=0;i--){
+            if(i == digits.size()-1){
+                ans.push_back((digits[i]+1)%10);
+                carry = (digits[i]+1)/10;
+            }
+            else{
+                ans.push_back((digits[i]+carry)%10);
+                carry = (digits[i]+carry)/10;
+            }
+            
+        }
+
+        if(carry > 0){
+            ans.push_back(carry);
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
